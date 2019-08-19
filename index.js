0c17eaa3abd7004af606aa79669657f49673dd61
Sophonias Isaac <sophonias8@gmail.com>
2019-08-19 15:39:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,34 @@ searchForm.addEventListener('submit', e => {
 	
 	//search reddit 
 	
-	reddit.search(searchTerm, searchLimit, sortBy); 
+	reddit.search(searchTerm, searchLimit, sortBy)
+	.then(results =>{
+		console.log(results);
+		let output = '<div class ="card-columns">';
+		//loop through posts 
+		results.forEach(post => {
+			//check for image
+		const image = post.preview ? post.preview .images[0]	
+		.source.url : 'https://cdn.comparitech.com/wp-content/uploads/2017/08/reddit-1.jpg'
+			output +=`
+<div class="card">
+  <img src="${image}" class="card-img-top" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">${post.title}</h5>
+    <p class="card-text">${truncateText(post.selftext, 100)}</p>
+    <a href="${post.url}" target="_blank" class="btn btn-primary">Read more</a>
+<hr>
+<span class ="badge badge-secondary ">Subreddit: ${post.subreddit}</span>
+<span class ="badge badge-dark ">Score: ${post.score}</span>
+  </div>
+</div>
+
+`
+		});
+		output += '</div>';
+		document.getElementById('results').innerHTML = output; 
+		
+	}); 
 	
 				e.preventDefault(); 	
 	
@@ -65,4 +92,12 @@ function showMessage(message, className){
 	//Timeout alert remove alert 
 	setTimeout(() => document.querySelector('.alert').remove
 			  (), 3000);
+}
+
+
+//trunctate text 
+function truncateText(text, limit) {
+	const shortned = text.indexOf(' ', limit);
+	if(shortned == -1) return text; 
+	return text.substring(0, shortned);
 }
